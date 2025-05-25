3f13861de6b1810b547e04f24abaa61822188d81
JidexBD <ewuolajide7@gmail.com>
2025-05-25 15:10:01 +0100

    same full erc-20 token

--- Contracts/MyERC-20.sol
+++ Contracts/MyERC-20.sol
@@ -1,11 +1,18 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.26;
-import { ERC20 } from "@openzeppelin/contracts@5.2.0/token/ERC20/ERC20.sol";
+pragma solidity ^0.8.19;
 
-contract MyERC20 is ERC20 {
-    constructor() ERC20("My Cyfrin CLF Token", "CLF") {}
+import { ERC20 } from "@openzeppelin/contracts@4.6.0/token/ERC20/ERC20.sol";
+import { AccessControl } from "@openzeppelin/contracts@4.6.0/access/AccessControl.sol";
 
-    function mint(address to, uint256 amount) public {
+contract MyERC20 is ERC20, AccessControl {
+   bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    constructor() ERC20("My Cyfrin CLF Token", "CLF") {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);    
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
         _mint(to, amount);
     }
 }
